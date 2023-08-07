8eed196e22d8a5fbb27accbfcfcc60757923a6b3
hamedsafari <hamed.safari1990@gmail.com>
2023-08-07 17:04:19 +0330

    created first version of recorder.

---
+++ recorder.js
@@ -0,0 +1,71 @@
+var Xvfb = require('xvfb');
+var puppeteer = require('puppeteer');
+const { spawn, spawnSync } = require('child_process');
+
+async function record(url, username, password) {
+    var xvfb = new Xvfb({
+        reuse: false,
+        xvfb_args: [
+            "-screen", "0", "1920x1080x24",
+            "-ac",
+            "-nolisten", "tcp",
+            "-dpi", "96",
+            "+extension", "RANDR"
+        ]
+    });
+    xvfb.startSync();
+
+    var browser = await puppeteer.launch({
+        headless: false,
+        executablePath: '/usr/bin/google-chrome',
+        defaultViewport: {width: 1920, height: 1080},
+        ignoreDefaultArgs: [
+            "--mute-audio",
+        ],
+        args: [
+            "--no-sandbox",
+            "--use-fake-ui-for-media-stream",
+            "--window-size=1920,1080",
+            "--start-fullscreen",
+            "--disable-notifications",
+        ]
+    });
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.locator("input#username").fill(username);
+    await page.locator("input#password").fill(password);
+    await page.click(".login-btn");
+    const listenerButton = await page.waitForSelector("div >>> ::-p-text(ورود به عنوان شنونده)");
+    await listenerButton.click();
+
+    var time = new Date().getTime();
+
+    var options = [
+        "-video_size", "1920x1080",
+        "-framerate", "30",
+        "-f", "x11grab",
+        "-draw_mouse", "0",
+        "-i", xvfb._display,
+        "-f", "pulse",
+        "-ac", "2",
+        "-i", "default",
+        "/output/video/video" + time + ".mkv"
+    ];
+
+    var cmd = 'ffmpeg';
+    var proc = spawn(cmd, options);
+
+    proc.stdout.on('data', function (data) {
+        console.log(data);
+    });
+
+    proc.stderr.setEncoding("utf8")
+    proc.stderr.on('data', function (data) {
+        console.log(data);
+    });
+
+    proc.on('close', async function () {
+        console.log('finished');
+        xvfb.stopSync();
+    });
+}
